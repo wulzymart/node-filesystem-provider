10fa3738ede2311cbbb0ed7486ce5b5c2c13161b
Olawole Olawale Martins <Olawalemart@gmail.com>
2024-06-22 14:04:35 +0100

    made changes to docker image

--- filesystem-server.js
+++ filesystem-server.js
@@ -1994,6 +1994,3 @@ var server = app.listen(runPort, function () {
   var port = server.address().port;
   console.log("Example app listening at http://%s:%s", host, port);
 });
-server.on("request", (data) => {
-  console.log(data.url, data.method);
-});
