e3270c9d5348df70c534fbfb47d83dcd821a583b
Olawole Olawale Martins <Olawalemart@gmail.com>
2024-07-10 23:32:04 +0100

    updated the rate limit

--- filesystem-server.js
+++ filesystem-server.js
@@ -5,7 +5,7 @@ var app = express();
 const rateLimit = require("express-rate-limit");
 // Enable rate limiting
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
+  windowMs: 5 * 60 * 1000, // 5 minutes
   max: 100, // limit each IP to 100 requests per windowMs
 });
 var size = 0;
